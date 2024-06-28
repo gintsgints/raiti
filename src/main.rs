113d0e1cc28100d592e2e754fbaf5201c292bd38
Gints Polis <polis.gints@gmail.com>
2024-06-28 14:05:04 +0300

    adding core

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{column, container, text, text_editor};
+use iced::{Element, Renderer, Sandbox, Settings, Theme};
+
+fn main() -> iced::Result {
+    Editor::run(Settings::default())
+}
+
+struct Editor {
+    content: text_editor::Content,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action),
+}
+
+impl Sandbox for Editor {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self {
+            content: text_editor::Content::new(),
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("Cool editor...")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {
+            Message::Edit(action) => self.content.perform(action),
+        }
+    }
+
+    fn view(&self) -> Element<'_, Self::Message> {
+        let input = text_editor(&self.content).on_action(Message::Edit);
+        container(input).padding(10).into()
+    }
 }
